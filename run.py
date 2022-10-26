7700e7d8f9cabbef3a8b0a1da9152da4d1a27870
Mikaela Grace <gracemikaela@gmail.com>
2022-10-25 20:19:32 -0500

    add example run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+from src.example import Example
+
+
+print("Part 1")
+print("POSITIVE")
+print("NEGATIVE")
+print("PART 2")
+print("plots printed here!")
+print("PART 3")
+print("Bleu Scores here!")
